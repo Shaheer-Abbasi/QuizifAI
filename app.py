647e6584182e8c6780a82705e7f0a501333d08a6
Shaheer <abbasi.shaheer14@gmail.com>
2025-07-24 23:15:03 -0500

    fix reload with ajax and jquery for user input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import requests
 
 app = Flask(__name__)
@@ -9,6 +9,11 @@ def index():
     if request.method == "POST":
         user_input = request.form.get("study_material")
         print(f"User entered: {user_input}") 
+
+        if request.headers.get("X-Requested-With") == "XMLHttpRequest":
+            return jsonify({"status": "success", "input" : user_input})
+        else:
+            return render_template("index.html")
     return render_template("index.html", user_input=user_input)
 
 if __name__ == "__main__":
