1d3c721e421f3f6031e44eb1629790c6d1f190b5
Shaheer <abbasi.shaheer14@gmail.com>
2025-07-26 16:24:41 -0500

    anonymize api key

--- app.py
+++ app.py
@@ -3,27 +3,20 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from openai import OpenAI
 import logging
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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",
+    api_key=os.getenv("OPENAI_API_KEY")
 )
+
+load_dotenv()
 
 @app.route("/", methods=["POST", "GET"])
 def index():
-    completion = client.chat.completions.create(
-    model="deepseek/deepseek-chat-v3-0324:free",
-    messages=[
-        {
-        "role": "user",
-        "content": "When did Napoleon Bonaparte become Emperor of the French?"
-        }
-    ]
-    )
-    logging.basicConfig(level=logging.INFO)
-    logging.info(completion.choices[0].message.content)
-    
     user_input = None
     if request.method == "POST":
         user_input = request.form.get("study_material")
@@ -33,6 +26,19 @@ def index():
             return jsonify({"status": "success", "input" : user_input})
         else:
             return render_template("index.html")
+        
+    completion = client.chat.completions.create(
+    model="deepseek/deepseek-chat-v3-0324:free",
+    messages=[
+        {
+        "role": "user",
+        "content": "Give me 5 facts about the following " + user_input if user_input else "say 'No input provided'"
+        }
+    ]
+    )
+    logging.basicConfig(level=logging.INFO)
+    logging.info(completion.choices[0].message.content)
+
     return render_template("index.html", user_input=user_input)
 
 if __name__ == "__main__":
